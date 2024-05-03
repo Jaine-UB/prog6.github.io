aa7297197480f930b9183dfb3286088a87736f1b
Jaine <114710646+Jaine-UB@users.noreply.github.com>
2024-05-03 16:01:17 -0400

    uploading ticketApp.js

---
+++ ticketApp.js
@@ -0,0 +1,30 @@
+let app = Vue.createApp({
+    data() {     // returns an object of data
+        return {
+            results: [], 
+            tickets: [], 
+            showTable:false      // all data items retrieved from the json file
+        }
+    },
+	created() {      // section executes as page loads
+		var d = new Date()
+		this.time_loaded = d.toLocaleTimeString()
+		
+		this.getData()  // call method to load json data as page loads
+	},
+    methods: {    // an object of functions (methods)
+        async getData() {
+            const response = await fetch('tickets1.json')
+            this.results = await response.json()
+        },
+		getTickets() {
+            this.showTable = true
+			// filter data form given ticket
+			this.tickets = this.results.filter( (ticket) => item.ticket == this.ticket );
+			
+		}
+   },
+    
+})
+
+app.mount('#main')   //in which div to mount the vue app
